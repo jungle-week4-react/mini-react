21ab109e05604a3dde2b45bf8d510c3ff5223b51
gannak1 <gi676788@gmail.com>
2026-03-10 15:06:55 +0900

    IPv6 완료

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,41 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+"""
+중간에 값 넣는 것
+
+"""
+
+input_text = input()
+
+
+def function(input_text):
+    result = ""
+    split_text = input_text.split(":")
+    for i in range(len(split_text)):
+        if i != len(split_text) - 1:
+            result = result + ("000000" + split_text[i])[-4:] + ":"
+        else:
+            result = result + ("000000" + split_text[i])[-4:]
+    return [result, len(split_text)]
+
+
+if len(input_text.split("::")) > 1:
+    count = 0
+    blank_index = -1
+    split_double_text = input_text.split("::")
+    for i in range(len(split_double_text)):
+        if split_double_text[i] != "":
+            split_double_text[i], temp_count = function(split_double_text[i])
+            count += temp_count
+        else:
+            blank_index = i
+    if blank_index == -1:
+        split_double_text.insert(1, ":".join(["0000" for i in range(8 - count)]))
+    else:
+        split_double_text[blank_index] = ":".join(["0000" for i in range(8 - count)])
+    result = ":".join(split_double_text)
+
+else:
+    result = function(input_text)[0]
+print(result)

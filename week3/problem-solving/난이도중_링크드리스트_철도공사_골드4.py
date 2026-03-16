076ff77783ae54c56a45436e3f0f21f25535cf59
gannak1 <gi676788@gmail.com>
2026-03-16 17:50:53 +0900

    철도공사 완료

--- week3/problem-solving/난이도중_링크드리스트_철도공사_골드4.py
+++ week3/problem-solving/난이도중_링크드리스트_철도공사_골드4.py
@@ -4,30 +4,74 @@
 import sys
 
 n, m = list(map(int, sys.stdin.readline().rstrip("\n").split()))
-station_num_list = sys.stdin.readline().rstrip("\n").split()
-
-work_list = []
-for _ in range(m):
-    work_list.append(sys.stdin.readline().rstrip("\n").split())
+station_num_list = list(map(int,sys.stdin.readline().rstrip("\n").split()))
 
 
-
-class Node:
-    def __init__(self, station_num):
-        self.station_num = station_num
-        self.next = None
-        self.prev = None
+# class Node:
+#     def __init__(self, station_num):
+#         self.station_num = station_num
+#         self.next = None
+#         self.prev = None
 
 
 class LinkedList:
-    def __init__(self,arr:list[str]):
-        a
-    
-    def bn_function(work):
-        i,j = work[1:]
-    def bp_function(work):
-        i,j = work[1:]
-    def cp_function(work):
-        i = work[1]
-    def cn_function(work):
-        i = work[1]
+    def __init__(self, arr: list[str]):
+        self.linked_list_next = [0] * 1000001
+        self.linked_list_prev = [0] * 1000001
+
+        self.first_node = None
+        cursor = None
+        for i in range(len(arr)):
+            data = arr[i]
+            if i == 0:
+                self.first_node = arr[i]
+                cursor = arr[i]
+            else:
+                self.node_connect(cursor, arr[i])
+                cursor = arr[i]
+        if len(arr) != 0:
+            self.node_connect(arr[i],self.first_node)
+
+    def node_connect(self, prev_index, next_index):
+        self.linked_list_next[prev_index] = next_index
+        self.linked_list_prev[next_index] = prev_index
+
+    def work_start(self, work):
+        if work[0] == "BN":
+            i, j = work[1:]
+            next_node = self.linked_list_next[i]
+            self.node_connect(i, j)
+            self.node_connect(j, next_node)
+            return str(next_node)
+        if work[0] == "BP":
+            i, j = work[1:]
+            prev_node = self.linked_list_prev[i]
+            self.node_connect(j, i)
+            self.node_connect(prev_node, j)
+            return str(prev_node)
+        if work[0] == "CN":
+            i = work[1]
+            origin_prev = self.linked_list_next[i]
+            self.node_connect(i,self.linked_list_next[self.linked_list_next[i]])
+            return str(origin_prev)
+        if work[0] == "CP":
+            i = work[1]
+            origin_next = self.linked_list_prev[i]
+            self.node_connect(self.linked_list_prev[self.linked_list_prev[i]],i)
+            return str(origin_next)
+
+
+linked_list = LinkedList(station_num_list)
+
+data_store = []
+
+for _ in range(m):
+    cmd, *rest = sys.stdin.readline().split()
+    if cmd in ("BN", "BP"):
+        appending_data = [cmd, int(rest[0]), int(rest[1])]
+    else:
+        appending_data = [cmd, int(rest[0])]
+
+    data_store.append(linked_list.work_start(appending_data))
+
+sys.stdout.write("\n".join(data_store))

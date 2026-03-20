824776ce5c784d58182e7bed6191abe52d85fdc2
gannak1 <gi676788@gmail.com>
2026-03-20 23:10:05 +0900

    바이러스 완료

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,32 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+from collections import deque
+
+
+total_computer = int(input())
+number_of_connect = int(input())
+list_of_connect = []
+graph = {}
+for _ in range(number_of_connect):
+    appending_list = list(map(int,input().split()))
+    list_of_connect.append(appending_list)
+    if appending_list[0] not in graph:
+        graph[appending_list[0]] = []
+    if appending_list[1] not in graph:
+        graph[appending_list[1]] = []
+    graph[appending_list[0]].append(appending_list[1])
+    graph[appending_list[1]].append(appending_list[0])
+
+queue = deque([1])
+visited = []
+
+while len(queue) != 0:
+    q = queue.popleft()
+    if q not in visited:
+        visited.append(q)
+        if q in graph:
+            for i in graph[q]:
+                if i not in visited:
+                    queue.append(i)
+
+print(len(visited)-1)

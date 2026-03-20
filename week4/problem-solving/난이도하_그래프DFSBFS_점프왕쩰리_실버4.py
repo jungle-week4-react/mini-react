d90d8d75a177ec04ec3c891201e0dfef2281491a
gannak1 <gi676788@gmail.com>
2026-03-20 21:51:30 +0900

    점프 완료

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,3 +1,35 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
 # 점프 = 두칸이동
+
+sector_shape = int(input())
+sector = []
+for _ in range(sector_shape):
+    sector.append(list(map(int,input().split())))
+
+success = "HaruHaru"
+fail = "Hing"
+
+
+def main(arr):
+
+    status = ["Hing"]
+
+    def recursive(arr, row, col):
+        if row > len(arr)-1 or col > len(arr)-1:
+            return False
+        elif row == len(arr)-1 and col == len(arr)-1:
+            status[0] = "HaruHaru"
+            return True
+        jump_power = arr[col][row]
+        if jump_power == 0:
+            return False
+        for array in [[row, col + jump_power], [row + jump_power, col]]:
+            i, j = array
+            recursive(arr,i,j)
+            
+    recursive(arr,0,0)
+    return status[0]
+
+
+print(main(sector))
--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,6 +1,8 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
 
+from collections import deque
+
 test_number = int(input())
 for _ in range(test_number):
     country, plane = list(map(int,input().split()))
@@ -16,4 +18,21 @@ for _ in range(test_number):
         graph[plane_root[0]].append(plane_root[1])
         graph[plane_root[1]].append(plane_root[0])
     
-    1
+    start = list(graph.keys())[0]
+    visited = []
+    queue = deque([start])
+    max_plane_number = [100000]
+    depth = [0]
+    def recursive(graph, q):
+        if len(list(set(visited))) == country and max_plane_number[0] >= depth[0]:
+            max_plane_number[0] = depth[0]
+            return
+        for i in graph[q]:
+            if i not in visited:
+                depth[0] += 1
+                visited.append(i)
+                recursive(graph, i)
+                depth[0] -= 1
+                visited.pop()
+    recursive(graph,queue[0])
+    print(max_plane_number[0]-1)

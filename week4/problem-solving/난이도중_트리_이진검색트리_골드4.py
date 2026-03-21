9892fb6006a155db29f9e012782ef4bd23f22cf6
gannak1 <gi676788@gmail.com>
2026-03-21 13:56:05 +0900

    이진검색 완료

--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,89 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+
+# *
+
+# class Node:
+#     def __init__(self, value, next, prev):
+#         self.value = value
+#         self.next = next
+#         self.prev = prev
+#         self.parent = None
+
+#     def next_connect(self, next_node):
+#         self.next.append(next_node)
+#         next_node.parent = self
+
+#     def prev_connect(self, prev_node):
+#         self.prev.append(prev_node)
+#         prev_node.parent = self
+
+# def binary_back_seach(present_node):
+#     if present_node.prev:
+#         binary_back_seach(present_node.prev[0])
+
+#     if present_node.next:
+#         binary_back_seach(present_node.next[0])
+
+#     return print(present_node.value)
+
+# def binary_maker(present_node, node):
+#     if present_node.value >= node.value:
+#         if present_node.prev:
+#             binary_maker(present_node.prev[0],node)
+#         else:
+#             present_node.prev_connect(node)
+#     else:
+#         if present_node.next:
+#             binary_maker(present_node.next[0],node)
+#         else:
+#             present_node.next_connect(node)
+
+# def main(arr):
+#     for i in range(len(arr)):
+#         new_node = Node(arr[i], [], [])
+#         if i == 0:
+#             first_node = new_node
+#         else:
+#             binary_maker(first_node,new_node)
+#     first_node.length = i + 1
+#     return first_node
+
+
+import sys
+
+
+def postorder(start, end):
+    # 구간이 비면 종료
+    if start == end:
+        print(arr[start])
+        return
+    if start > end:
+        return
+
+    # start 위치 값을 루트로 잡기
+    root = arr[start]
+
+    # 루트보다 큰 값이 처음 나오는 위치 찾기
+    bigger_index = end+1
+    for i in range(start+1, end + 1):
+        if root < arr[i]:
+            bigger_index = i
+            break
+
+    # 왼쪽 구간 재귀
+    postorder(start+1, bigger_index-1)
+    # 오른쪽 구간 재귀
+    postorder(bigger_index, end)
+
+    # 루트 출력
+    print(root)                                                      
+
+
+arr = []
+for line in sys.stdin:
+    arr.append(int(line.rstrip()))
+
+# arr = [50, 30, 24, 5, 28, 45, 98, 52, 60]
+sys.setrecursionlimit(10**6)
+postorder(0, len(arr) - 1)
--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,35 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+
+start = 1
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+number_of_connect = int(input().rstrip())
+
+connect_list = []
+graph = {}
+
+for _ in range(number_of_connect-1):
+    data = list(map(int,input().rstrip().split()))
+    connect_list.append(data)
+    if data[0] not in graph:
+        graph[data[0]] = []
+    if data[1] not in graph:
+        graph[data[1]] = []
+    graph[data[0]].append(data[1])
+    graph[data[1]].append(data[0])
+
+queue = deque([start])
+visited = set()
+depth = [-1] * number_of_connect
+while queue:
+    q = queue.popleft()
+    if q not in visited:
+        visited.add(q)
+        for i in  graph[q]:
+            if i not in visited:
+                queue.append(i)
+                depth[i-1] = str(q)
+print("\n".join(depth[1:]))

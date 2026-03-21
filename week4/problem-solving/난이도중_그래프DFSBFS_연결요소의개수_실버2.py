0019de1a6451c5c69adfd1dd89eaa3f1c53e728d
gannak1 <gi676788@gmail.com>
2026-03-21 19:09:46 +0900

    연결개수 완료

--- week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
+++ week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
@@ -9,22 +9,32 @@ number_of_data = list(map(int, input().rstrip().split()))
 
 edges = []
 graph = {}
+
+for i in range(1,number_of_data[0]+1):
+    graph[i] = []
+
 for _ in range(number_of_data[-1]):
     edge = list(map(int, input().rstrip().split()))
-    if edge[0] not in graph:
-        graph[edge[0]] = []
-    if edge[1] not in graph:
-        graph[edge[1]] = []
     graph[edge[0]].append(edge[1])
     graph[edge[1]].append(edge[0])
     edges.append(edge)
-queue = deque([edges[0][0]])
-visited = []
-while True:
-    q = queue.popleft()
-    if q not in visited:
-        visited.append(q)
-        for i in graph[q]:
-            if i not in visited:
-                queue.append(i)
-        
+
+graph_number = 0
+visited = set()
+while len(visited) < number_of_data[0]:
+    queue = deque([])
+    for i in range(1,number_of_data[0]+1):
+        if i not in visited:
+            queue.append(i)
+            break
+    graph_number += 1
+    while len(visited) < number_of_data[0]:
+        q = queue.popleft()
+        if q not in visited:
+            visited.add(q)
+            for i in graph[q]:
+                if i not in visited:
+                    queue.append(i)
+        if not queue:
+            break
+print(graph_number)
